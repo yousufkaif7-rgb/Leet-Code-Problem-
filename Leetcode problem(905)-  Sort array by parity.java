a7647ef3fd5c21081d3ecf20b2dbee3314cf1024
kaifmohamed685-ui <yousufkaif7@gmail.com>
2026-07-20 10:44:36 +0530

    Create Leetcode problem(905)-  Sort array by parity.java

---
+++ Leetcode problem(905)-  Sort array by parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+     int left = 0;
+     int right = nums.length-1;
+     while(left<right){
+        if(nums[left]%2==0){
+            left++;
+        }
+     else if(nums[right]%2!=0){
+        right--;
+     }   
+     else{
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+     }
+     }  
+     return nums; 
+    }
+}
