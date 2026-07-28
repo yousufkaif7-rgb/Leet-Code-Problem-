4ef054b4406fa4c1ef78277dbac8131190f4597a
kaifmohamed685-ui <yousufkaif7@gmail.com>
2026-07-28 10:18:07 +0530

    Time: 14 ms (16.25%), Space: 44.2 MB (39.63%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.CountDownLatch;
+
+class Foo {
+
+    private final CountDownLatch latch1;
+    private final CountDownLatch latch2;
+
+    public Foo() {
+        latch1 = new CountDownLatch(1);
+        latch2 = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        latch1.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        latch1.await();
+        printSecond.run();
+        latch2.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        latch2.await();
+        printThird.run();
+    }
+}
