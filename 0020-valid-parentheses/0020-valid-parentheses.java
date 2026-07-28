95c2ebfb4ecf06ca45079b0b92bb3a6cf1bf5698
kaifmohamed685-ui <yousufkaif7@gmail.com>
2026-07-28 11:26:58 +0530

    Time: 3 ms (86.21%), Space: 43.4 MB (41.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
